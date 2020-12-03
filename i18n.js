968d34b31df681f52ffe7542017b1acece07b832
stefan2904 <stefan2904@users.noreply.github.com>
2020-12-03 17:18:45 +0000

    Deploying to gh-pages from  @ 05cd00d55d6b96d9d349dc6af59973f2f7738947 🚀

--- i18n.js
+++ i18n.js
@@ -2,5 +2,5 @@
  * License: LGPL-2.1-or-later
  * Dependencies:
  */
-import{c as t}from"./shared/i18next.f6eb636c.es.js";const e=t({en:{"starter-activity":{title:"Starter Activity"}},de:{"starter-activity":{title:"Starter Activity"}}},"de","en");function r(){return e.cloneInstance()}export{r as c};
+import{c as t}from"./shared/i18next.f6eb636c.es.js";const e=t({en:{"starter-activity":{title:"Starter Activity"}},de:{"starter-activity":{title:"Starter Activity"}}},"en","de");function r(){return e.cloneInstance()}export{r as c};
 //# sourceMappingURL=i18n.js.map
