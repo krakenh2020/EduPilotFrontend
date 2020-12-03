06703cc2c337d8b2dda3e6797d9135a28fee464b
Stefan More <stefan.more@iaik.tugraz.at>
2020-12-03 18:16:10 +0100

    i18n

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import {createInstance} from '@dbp-toolkit/common/i18next.js';
 import de from './i18n/de/translation.json';
 import en from './i18n/en/translation.json';
 
-const i18n = createInstance({en: en, de: de}, 'de', 'en');
+const i18n = createInstance({en: en, de: de}, 'en', 'de');
 
 export function createI18nInstance () {
     return i18n.cloneInstance();
