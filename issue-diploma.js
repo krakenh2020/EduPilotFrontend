2a3bffd2dfc92baa02b214d14db038150ce1e31c
stefan2904 <stefan2904@users.noreply.github.com>
2021-03-24 16:48:26 +0000

    Deploying to gh-pages from  @ 3127dd18915fdd0a9403606747492b4fbee844a9 🚀

--- issue-diploma.js
+++ issue-diploma.js
@@ -1,4 +1,4 @@
-let t,e,i,s,a,r=t=>t;function n(){return(n=Object.assign||function(t){for(var e=1;e<arguments.length;e++){var i=arguments[e];for(var s in i)Object.prototype.hasOwnProperty.call(i,s)&&(t[s]=i[s])}return t}).apply(this,arguments)}
+let t,e,i,r,s,a=t=>t;function n(){return(n=Object.assign||function(t){for(var e=1;e<arguments.length;e++){var i=arguments[e];for(var r in i)Object.prototype.hasOwnProperty.call(i,r)&&(t[r]=i[r])}return t}).apply(this,arguments)}
 /*!
  * License: LGPL-2.1-or-later
  * Dependencies:
@@ -12,7 +12,7 @@ let t,e,i,s,a,r=t=>t;function n(){return(n=Object.assign||function(t){for(var e=
  * webcomponent-qr-code: MIT
  * @babel/runtime: MIT
  * i18next: MIT
- */import{c as l}from"./i18n.js";import{S as p,A as o,I as c,B as d,c as h,g as m,h as u,d as y}from"./shared/logger.da18e832.es.js";import{Q as g}from"./shared/qr-code.7c6f0a91.es.js";import"./shared/i18next.e2c56140.es.js";const b=l();class I extends(p(o)){constructor(){super(),this.auth={},this.lang=b.language,this.exporting=!1,this.fetchDiplomas().then((t=>{this.diplomas=t}))}static get scopedElements(){return{"dbp-icon":c,"dbp-button":d,"dbp-qr-code":g}}static get properties(){return n({},super.properties,{lang:{type:String},exporting:{type:Boolean,attribute:!1},exportingId:{type:String},diplomas:{type:Array},auth:{type:Object}})}connectedCallback(){super.connectedCallback()}update(t){t.forEach(((t,e)=>{switch(e){case"lang":b.changeLanguage(this.lang)}})),super.update(t)}static get styles(){return h(t||(t=r`
+ */import{c as l}from"./i18n.js";import{S as o,A as p,I as c,B as d,c as h,g as u,h as m,d as y}from"./shared/logger.da18e832.es.js";import{Q as g}from"./shared/qr-code.7c6f0a91.es.js";import"./shared/i18next.e2c56140.es.js";const b=l();class I extends(o(p)){constructor(){super(),this.auth={},this.entryPointUrl="",this.lang=b.language,this.exporting=!1}static get scopedElements(){return{"dbp-icon":c,"dbp-button":d,"dbp-qr-code":g}}static get properties(){return n({},super.properties,{lang:{type:String},exporting:{type:Boolean,attribute:!1},exportingId:{type:String},diplomas:{type:Array},auth:{type:Object},entryPointUrl:{type:String,attribute:"entry-point-url"}})}connectedCallback(){super.connectedCallback()}update(t){t.forEach(((t,e)=>{switch(e){case"lang":b.changeLanguage(this.lang);break;case"entryPointUrl":this.fetchDiplomas().then((t=>{this.diplomas=t}))}})),super.update(t)}static get styles(){return h(t||(t=a`
             ${0}
             
             .vc-list {
@@ -25,9 +25,9 @@ let t,e,i,s,a,r=t=>t;function n(){return(n=Object.assign||function(t){for(var e=
               justify-content: space-between;
               margin-bottom: 1rem;
             }
-        `),m())}export(t){console.log("export"),this.exporting=!0,this.exportingId=t}async httpGetAsync(t,e){return await fetch(t,e).then((t=>{if(!t.ok)throw t;return t.json()}))}async fetchDiplomas(){const t={headers:{Authorization:"Bearer "+this.auth.token}};return(await this.httpGetAsync("http://127.0.0.1:8001/diplomas?page=1",t))["hydra:member"]}render(){if(!this.auth.token)return u(e||(e=r`
+        `),u())}export(t){console.log("export"),this.exporting=!0,this.exportingId=t}async httpGetAsync(t,e){return await fetch(t,e).then((t=>{if(!t.ok)throw t;return t.json()}))}async fetchDiplomas(){const t={headers:{Authorization:"Bearer "+this.auth.token}},e=this.entryPointUrl+"/diplomas?page=1";return(await this.httpGetAsync(e,t))["hydra:member"]}render(){if(!this.auth.token)return m(e||(e=a`
                 <p>${0}</p>
-            `),b.t("please-login"));if(!this.exporting){const t=this.diplomas.map((t=>u(i||(i=r`
+            `),b.t("please-login"));if(!this.exporting){const t=this.diplomas.map((t=>m(i||(i=a`
                 <li>
                     <div>
                         <strong>${0}</strong><br />
@@ -36,11 +36,11 @@ let t,e,i,s,a,r=t=>t;function n(){return(n=Object.assign||function(t){for(var e=
                     </div>
                     <dbp-button type="is-primary" value="Export" no-spinner-on-click="true" @click="${0}" />
                 </li>
-            `),t.name,t.academicDegree,t.achievenmentDate,(()=>this.export(t["@id"])))));return u(s||(s=r`
+            `),t.name,t.academicDegree,t.achievenmentDate,(()=>this.export(t["@id"])))));return m(r||(r=a`
                 <ul class="vc-list">
                     ${0}
                 </ul>
-            `),t)}return u(a||(a=r`
+            `),t)}return m(s||(s=a`
             <p>
                 ${0}
             </p>
--- issue-grades.js
+++ issue-grades.js
@@ -1,4 +1,4 @@
-let e,t,r,s,i,a=e=>e;function n(){return(n=Object.assign||function(e){for(var t=1;t<arguments.length;t++){var r=arguments[t];for(var s in r)Object.prototype.hasOwnProperty.call(r,s)&&(e[s]=r[s])}return e}).apply(this,arguments)}
+let t,e,r,s,i,a=t=>t;function n(){return(n=Object.assign||function(t){for(var e=1;e<arguments.length;e++){var r=arguments[e];for(var s in r)Object.prototype.hasOwnProperty.call(r,s)&&(t[s]=r[s])}return t}).apply(this,arguments)}
 /*!
  * License: LGPL-2.1-or-later
  * Dependencies:
@@ -12,7 +12,7 @@ let e,t,r,s,i,a=e=>e;function n(){return(n=Object.assign||function(e){for(var t=
  * webcomponent-qr-code: MIT
  * @babel/runtime: MIT
  * i18next: MIT
- */import{c as l}from"./i18n.js";import{S as o,A as c,I as p,B as d,c as h,g as u,h as g,d as y}from"./shared/logger.da18e832.es.js";import{Q as m}from"./shared/qr-code.7c6f0a91.es.js";import"./shared/i18next.e2c56140.es.js";const b=l();class I extends(o(c)){constructor(){super(),this.auth={},this.lang=b.language,this.exporting=!1,this.fetchCourseGrades().then((e=>{this.courseGrades=e}))}static get scopedElements(){return{"dbp-icon":p,"dbp-button":d,"dbp-qr-code":m}}static get properties(){return n({},super.properties,{lang:{type:String},exporting:{type:Boolean,attribute:!1},exportingId:{type:String},courseGrades:{type:Array},auth:{type:Object}})}connectedCallback(){super.connectedCallback()}update(e){e.forEach(((e,t)=>{switch(t){case"lang":b.changeLanguage(this.lang)}})),super.update(e)}static get styles(){return h(e||(e=a`
+ */import{c as l}from"./i18n.js";import{S as o,A as c,I as p,B as d,c as h,g as u,h as y,d as g}from"./shared/logger.da18e832.es.js";import{Q as m}from"./shared/qr-code.7c6f0a91.es.js";import"./shared/i18next.e2c56140.es.js";const b=l();class I extends(o(c)){constructor(){super(),this.auth={},this.entryPointUrl="",this.lang=b.language,this.exporting=!1}static get scopedElements(){return{"dbp-icon":p,"dbp-button":d,"dbp-qr-code":m}}static get properties(){return n({},super.properties,{lang:{type:String},exporting:{type:Boolean,attribute:!1},exportingId:{type:String},courseGrades:{type:Array},auth:{type:Object},entryPointUrl:{type:String,attribute:"entry-point-url"}})}connectedCallback(){super.connectedCallback()}update(t){t.forEach(((t,e)=>{switch(e){case"lang":b.changeLanguage(this.lang);break;case"entryPointUrl":this.fetchCourseGrades().then((t=>{this.courseGrades=t}))}})),super.update(t)}static get styles(){return h(t||(t=a`
             ${0}
 
             .vc-list {
@@ -25,9 +25,9 @@ let e,t,r,s,i,a=e=>e;function n(){return(n=Object.assign||function(e){for(var t=
                 justify-content: space-between;
                 margin-bottom: 1rem;
             }
-        `),u())}export(e){console.log("export"),this.exporting=!0,this.exportingId=e}async httpGetAsync(e,t){return await fetch(e,t).then((e=>{if(!e.ok)throw e;return e.json()}))}async fetchCourseGrades(){const e={headers:{Authorization:"Bearer "+this.auth.token}};return(await this.httpGetAsync("http://127.0.0.1:8000/course_grades?page=1",e))["hydra:member"]}render(){if(!this.auth.token)return g(t||(t=a`
+        `),u())}export(t){console.log("export"),this.exporting=!0,this.exportingId=t}async httpGetAsync(t,e){return await fetch(t,e).then((t=>{if(!t.ok)throw t;return t.json()}))}async fetchCourseGrades(){const t={headers:{Authorization:"Bearer "+this.auth.token}},e=this.entryPointUrl+"/course_grades?page=1";return(await this.httpGetAsync(e,t))["hydra:member"]}render(){if(!this.auth.token)return y(e||(e=a`
                 <p>${0}</p>
-            `),b.t("please-login"));if(!this.exporting){const e=this.courseGrades.map((e=>g(r||(r=a`
+            `),b.t("please-login"));if(!this.exporting){const t=this.courseGrades.map((t=>y(r||(r=a`
                 <li>
                     <div>
                         <strong>${0}</strong><br />
@@ -37,11 +37,11 @@ let e,t,r,s,i,a=e=>e;function n(){return(n=Object.assign||function(e){for(var t=
                     </div>
                     <dbp-button type="is-primary" value="Export" no-spinner-on-click="true" @click="${0}" />
                 </li>
-            `),e.name,e.credits,e.grade,e.achievenmentDate,(()=>this.export(e["@id"])))));return g(s||(s=a`
+            `),t.name,t.credits,t.grade,t.achievenmentDate,(()=>this.export(t["@id"])))));return y(s||(s=a`
                 <ul class="vc-list">
                     ${0}
                 </ul>
-            `),e)}return g(i||(i=a`
+            `),t)}return y(i||(i=a`
             <p>
                 ${0}
             </p>
@@ -62,5 +62,5 @@ let e,t,r,s,i,a=e=>e;function n(){return(n=Object.assign||function(e){for(var t=
                 <li><a href="http://minerva.digital/" target="_blank">Minerva Wallet</a></li>
                 <li>Browser wallet</li>
             </ul>
-        `),b.t("issue-grades.scan"),JSON.stringify(this.courseGrades.filter((e=>e["@id"]===this.exportingId))[0],null,2),"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJ2YyI6eyJjcmVkZW50aWFsU3ViamVjdCI6eyJkZWdyZWUiOnsidHlwZSI6IkJhY2hlbG9yRGVncmVlIiwibmFtZSI6IkJhY2hlbG9yIG9mIFNjaWVuY2UgYW5kIEFydHMifX0sIkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIiwiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvZXhhbXBsZXMvdjEiXSwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIlVuaXZlcnNpdHlEZWdyZWVDcmVkZW50aWFsIl19LCJzdWIiOiIiLCJuYmYiOjE2MTI0MjY5OTYsImlzcyI6ImRpZDpldGhyOmFydGlzX3QxOjB4MWViOWEwZDk5YjE4Yjc4YjJmNjdhNDBmYTA5ZmRhODQ2MzVlZjk2NyJ9.7upzlCL3FJieO35TQa4_y9PlmEotXKphtRd9cstWt4Db2LICBl9RT3_aRl0aBRlHs29JJKQWEMSLwnWJOXsYAw",b.t("wallets"))}}y("issue-grades",I);
+        `),b.t("issue-grades.scan"),JSON.stringify(this.courseGrades.filter((t=>t["@id"]===this.exportingId))[0],null,2),"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJ2YyI6eyJjcmVkZW50aWFsU3ViamVjdCI6eyJkZWdyZWUiOnsidHlwZSI6IkJhY2hlbG9yRGVncmVlIiwibmFtZSI6IkJhY2hlbG9yIG9mIFNjaWVuY2UgYW5kIEFydHMifX0sIkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIiwiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvZXhhbXBsZXMvdjEiXSwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIlVuaXZlcnNpdHlEZWdyZWVDcmVkZW50aWFsIl19LCJzdWIiOiIiLCJuYmYiOjE2MTI0MjY5OTYsImlzcyI6ImRpZDpldGhyOmFydGlzX3QxOjB4MWViOWEwZDk5YjE4Yjc4YjJmNjdhNDBmYTA5ZmRhODQ2MzVlZjk2NyJ9.7upzlCL3FJieO35TQa4_y9PlmEotXKphtRd9cstWt4Db2LICBl9RT3_aRl0aBRlHs29JJKQWEMSLwnWJOXsYAw",b.t("wallets"))}}g("issue-grades",I);
 //# sourceMappingURL=issue-grades.js.map
