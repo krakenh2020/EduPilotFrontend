ecab5b459273a0f771a0038beed1f5e8d6827f53
Stefan More <stefan.more@iaik.tugraz.at>
2022-06-01 20:06:24 +0200

    setup karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,13 @@ module.exports = function(config) {
   config.set({
     basePath: 'dist',
     frameworks: ['mocha'],
+    plugins: [
+      'karma-coverage',
+      'karma-mocha',
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-sonarqube-reporter'),
+    ],
     client: {
       mocha: {
         ui: 'tdd',
@@ -11,6 +18,9 @@ module.exports = function(config) {
       {pattern: './*.js', included: true, watched: true, served: true, type: 'module'},
       {pattern: './**/*', included: false, watched: true, served: true},
     ],
+    preprocessors: {
+      'src/**/*.js': ['coverage']
+    },
     autoWatch: true,
     browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
     customLaunchers: {
@@ -20,6 +30,25 @@ module.exports = function(config) {
       }
     },
     singleRun: false,
-    logLevel: config.LOG_ERROR
+    logLevel: config.LOG_ERROR,
+    sonarqubeReporter: {
+      basePath: 'test',        // test files folder
+      filePattern: '*.js', // test files glob pattern
+      encoding: 'utf-8',          // test files encoding
+      outputFolder: 'reports',    // report destination
+      legacyMode: false,          // report for Sonarqube < 6.2 (disabled)
+      reportName: (metadata) => { // report name callback, but accepts also a 
+                                  // string (file name) to generate a single file
+        /**
+         * Report metadata array:
+         * - metadata[0] = browser name
+         * - metadata[1] = browser version
+         * - metadata[2] = plataform name
+         * - metadata[3] = plataform version
+         */
+         return metadata.concat('xml').join('.');
+      }
+    },
+    reporters: ['progress', 'coverage', 'sonarqube']
   });
 }
