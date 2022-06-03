b76ad491406394710f68c32ce84f28a1a9e47b16
Stefan More <stefan.more@iaik.tugraz.at>
2022-06-03 18:07:26 +0200

    cov config

--- karma.conf.js
+++ karma.conf.js
@@ -2,15 +2,15 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function(config) {
   config.set({
-    basePath: 'dist',
+    //basePath: 'dist',
     frameworks: ['mocha', 'chai'],
     plugins: [
       'karma-coverage',
       'karma-mocha',
       'karma-chai',
+      'karma-sourcemap-loader',
       require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
-      require('karma-sonarqube-reporter'),
+      require('karma-firefox-launcher')
     ],
     client: {
       mocha: {
@@ -19,14 +19,16 @@ module.exports = function(config) {
       },
     },
     files: [
-      {pattern: './*.js', included: true, watched: true, served: true, type: 'module'},
-      {pattern: './**/*', included: false, watched: true, served: true},
+      {pattern: 'dist/unit.js', included: true, watched: true, served: true, type: 'module'},
+      {pattern: 'test/unit.js', included: false, watched: true, served: true},
     ],
     preprocessors: {
-      './*.js': ['coverage']
+      'dist/*.js': ['sourcemap', 'coverage']
     },
     autoWatch: true,
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'], 
+    //browsers: ['FirefoxHeadless'], 
+    //browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless',  'FirefoxHeadless'],
     customLaunchers: {
         ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
@@ -35,25 +37,7 @@ module.exports = function(config) {
     },
     singleRun: false,
     logLevel: config.LOG_INFO,
-    // sonarqubeReporter: {
-    //   basePath: 'test',        // test files folder
-    //   filePattern: '*.js', // test files glob pattern
-    //   encoding: 'utf-8',          // test files encoding
-    //   outputFolder: 'reports',    // report destination
-    //   legacyMode: false,          // report for Sonarqube < 6.2 (disabled)
-    //   reportName: (metadata) => { // report name callback, but accepts also a 
-    //                               // string (file name) to generate a single file
-    //     /**
-    //      * Report metadata array:
-    //      * - metadata[0] = browser name
-    //      * - metadata[1] = browser version
-    //      * - metadata[2] = plataform name
-    //      * - metadata[3] = plataform version
-    //      */
-    //      return metadata.concat('xml').join('.');
-    //   }
-    // },
-    reporters: ['progress', 'coverage', 'sonarqube'],
+    reporters: ['progress', 'coverage'],
     coverageReporter: {
       type : 'lcov',
       dir : 'coverage/'
